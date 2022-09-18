65f504c75160d4642ca32fcf64ce25a99aaad1ab
OnetakeKG <86379850+OnetakeKG@users.noreply.github.com>
2022-09-18 14:41:14 +0300

    Initial commit

---
+++ app/src/main/java/ru/gb/car/MainViewModel.kt
@@ -0,0 +1,128 @@
+package ru.gb.car
+
+import android.graphics.*
+import android.view.SurfaceHolder
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
+import ru.gb.car.entity.Car
+import ru.gb.car.entity.Point
+import kotlin.math.*
+
+class MainViewModel : ViewModel() {
+    private var surfaceHolder: SurfaceHolder? = null
+    private lateinit var carBitmap: Bitmap
+    private lateinit var pointBitmap: Bitmap
+    private val car = Car()
+    private val point = Point()
+    private val matrix = Matrix()
+    private val scope = CoroutineScope(Dispatchers.Default)
+
+    fun setSurfaceHolder(surfaceHolder: SurfaceHolder?) {
+        this.surfaceHolder = surfaceHolder
+        val canvas = surfaceHolder?.lockCanvas()
+        if (canvas != null) {
+            car.x = (canvas.width / 2).toFloat()
+            car.y = (canvas.height / 2).toFloat()
+            car.angle = -90f
+            point.x = (canvas.width / 2).toFloat()
+            point.y = (canvas.height / 2).toFloat()
+            render(canvas)
+            surfaceHolder.unlockCanvasAndPost(canvas)
+        }
+    }
+
+    fun setBitmap(car: Bitmap, point: Bitmap) {
+        matrix.setScale(SCALING_FACTOR, SCALING_FACTOR)
+        carBitmap = Bitmap.createBitmap(car, 0, 0, car.width, car.height, matrix, false)
+        pointBitmap = Bitmap.createBitmap(point, 0, 0, point.width, point.height, matrix, false)
+    }
+
+    fun setPoint(x: Float, y: Float) {
+        point.x = x
+        point.y = y
+        carToPoint()
+    }
+
+    private fun carToPoint() {
+        scope.launch {
+            while (distance() > PIXEL_ARRIVAL_ACCURACY) {
+                val canvas = surfaceHolder?.lockCanvas()
+                if (canvas != null) {
+                    render(canvas)
+                    surfaceHolder?.unlockCanvasAndPost(canvas)
+                }
+                car.x = car.x + PIXEL_MACHINE_SPEED * cos(car.angle * DEGREES_TO_RADIANS)
+                car.y = car.y + PIXEL_MACHINE_SPEED * sin(car.angle * DEGREES_TO_RADIANS)
+                car.angle = car.angle + dAngle()
+                point.angle += POINT_ROTATION_SPEED
+            }
+        }
+    }
+
+    private fun dAngle(): Float {
+        val angle = atan2((point.y - car.y), (point.x - car.x)) * RADIANS_TO_DEGREES
+        if (abs(angle - car.angle) < MAX_ANGLE_OF_ROTATION) {
+            car.angle = angle
+            return 0f
+        }
+        if (abs(angle - car.angle) < 180) {
+            if ((angle - car.angle) < 0) return MAX_ANGLE_OF_ROTATION * -1
+            if ((angle - car.angle) > 0) return MAX_ANGLE_OF_ROTATION
+        }
+        if (abs(angle - car.angle) >= 180) {
+            if ((angle - car.angle) < 0) return MAX_ANGLE_OF_ROTATION
+            if ((angle - car.angle) > 0) return MAX_ANGLE_OF_ROTATION * -1
+        }
+        return 0f
+    }
+
+    private fun distance() =
+        sqrt((point.x - car.x) * (point.x - car.x) + (point.y - car.y) * (point.y - car.y))
+
+    private fun render(canvas: Canvas) {
+        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
+        drawPoint(canvas)
+        drawCar(canvas)
+    }
+
+    private fun drawCar(canvas: Canvas) {
+        matrix.setRotate(car.angle + 90)
+        val rotatedBitmap =
+            Bitmap.createBitmap(carBitmap, 0, 0, carBitmap.width, carBitmap.height, matrix, false)
+        canvas.drawBitmap(
+            rotatedBitmap,
+            car.x - rotatedBitmap.width.toFloat() / 2,
+            car.y - rotatedBitmap.height.toFloat() / 2,
+            null
+        )
+    }
+
+    private fun drawPoint(canvas: Canvas) {
+        matrix.setRotate(point.angle)
+        val rotatedBitmap =
+            Bitmap.createBitmap(
+                pointBitmap, 0, 0, pointBitmap.width, pointBitmap.height, matrix, false
+            )
+        canvas.drawBitmap(
+            rotatedBitmap,
+            point.x - rotatedBitmap.width.toFloat() / 2,
+            point.y - rotatedBitmap.height.toFloat() / 2,
+            null
+        )
+    }
+
+    override fun onCleared() {
+        surfaceHolder = null
+        super.onCleared()
+    }
+
+    companion object {
+        const val MAX_ANGLE_OF_ROTATION = 2f
+        const val PIXEL_MACHINE_SPEED = 8
+        const val POINT_ROTATION_SPEED = 3f
+        const val SCALING_FACTOR = 0.1f
+        const val PIXEL_ARRIVAL_ACCURACY = 40
+        const val RADIANS_TO_DEGREES = 180.0f / 3.14159f
+        const val DEGREES_TO_RADIANS = 3.14159f / 180.0f
+    }
+}
